bd45e6168fc34dc251e84a763bab6d66171c9a37
Miguel Almeida <darkmagusbr@gmail.com>
2016-09-19 00:01:08 -0300

    added quickstart project files

---
+++ README.md
@@ -0,0 +1,3 @@
+﻿Quickstart project based on Angular2 with webpack.
+
+https://angular.io/docs/ts/latest/guide/webpack.html
---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,19 @@
+﻿import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+describe('App', () => {
+    beforeEach(() => {
+        TestBed.configureTestingModule({ declarations: [AppComponent] });
+    });
+
+    it('should work', () => {
+        let fixture = TestBed.createComponent(AppComponent);
+        expect(fixture.componentInstance instanceof AppComponent).toBe(true, 'should create AppComponent');
+    });
+
+    it('name must be Joao', () => {
+        let fixture = TestBed.createComponent(AppComponent);
+        let comp = fixture.componentInstance;
+
+        expect(comp.test).toBe(12);
+    });
+});
